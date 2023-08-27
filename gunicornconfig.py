59a8ac061ba6777a85f44a632fd15e05160dcca5
Omar Khan <oakhan3@gmail.com>
2023-08-26 22:46:43 -0400

    feat: Update gunicorn config to start more workers by default and surface request logs.

--- gunicornconfig.py
+++ gunicornconfig.py
@@ -3,3 +3,16 @@ import os
 host = ""
 port = os.environ.get("APP_PORT", 5000)
 bind = f"{host}:{port}"
+workers = int(os.environ.get("WORKER_COUNT", 5))
+
+max_requests = 500
+max_requests_jitter = 50
+
+timeout = 300
+graceful_timeout = 300
+
+accesslog = "-"
+errorlog = "-"
+log_file = "-"
+
+preload_app = "RELOAD" not in os.environ
